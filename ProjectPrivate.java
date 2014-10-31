4d89bef22ff4ced5414091c0c3171eb84a0e2bbb
Trevor Holland <trevorholland@outlook.com>
2014-10-31 00:30:09 -0400

    Update to ProjectPrivate.java

--- ProjectPrivate.java
+++ ProjectPrivate.java
@@ -1,5 +1,32 @@
-class ProjectPrivate
+package ProjectPrivate;
 
-  public static void main(String[] args) {
-  
-  }
+import javax.swing.*;
+import java.awt.*;
+
+public class ProjectPrivateDriver {
+
+	public static void main(String[] args) {
+		
+		//Sets JFrame and components in it
+		JFrame frame = new JFrame("Project Private");
+		JLabel textLabel = new JLabel("System Temperatures",SwingConstants.CENTER);
+		textLabel.setPreferredSize(new Dimension(300, 300));
+		frame.getContentPane().add(textLabel, BorderLayout.CENTER);
+		// Adds Buttons to the JFrame
+		JPanel iconPanel = new JPanel();
+        JButton CPUTemp = new JButton("CPU Temperature");
+        JButton GPUTemp = new JButton("GPU Temperature");
+        JButton RAMTemp = new JButton("RAM Temperature");
+        iconPanel.add(CPUTemp);
+        iconPanel.add(GPUTemp);
+        iconPanel.add(RAMTemp);
+        frame.add(iconPanel);
+        frame.setLocationRelativeTo(null);
+		frame.pack();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+
+	}
+
+}
